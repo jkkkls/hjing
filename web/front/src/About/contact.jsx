fced50112098eaa2b60b31364e7620bad9783299
广波 <jkkkls@aliyun.com>
2024-01-24 15:18:57 +0800

    add front ui

---
+++ web/front/src/About/contact.jsx
@@ -0,0 +1,16 @@
+import { Typography } from "antd";
+const { Paragraph } = Typography;
+
+const ContactPage = () => {
+  return (
+    <div style={{ width: "90%", margin: "20px" }}>
+      <Typography>
+        <Paragraph>
+        公司地址：广州市天河区中山大道建工路13,15号天信楼5层513
+        </Paragraph>
+      </Typography>
+    </div>
+  );
+};
+
+export default ContactPage;
